413db4ed926aaeff942af3f852a2071d44584627
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2023-09-30 13:21:36 +0400

    Add https server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 require('dotenv').config();
 
+const fs = require('fs');
 const http = require('http');
+const https = require('https');
+
 const app = require('./src/app');
 
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(
+  {
+    cert: fs.readFileSync('cert/cert.pem'),
+    key: fs.readFileSync('cert/key.pem'),
+  },
+  app
+);
 
-server.listen(8080, function () {
+httpServer.listen(8080, function () {
   console.log('HTTP server is running on port 8080!');
+});
+
+httpsServer.listen(8083, function () {
+  console.log('HTTPS server is running on port 8083!');
 });
