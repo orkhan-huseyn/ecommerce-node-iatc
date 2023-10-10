41b1d318e758a6f6e46be6b7b5a09d5bf01c144c
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2023-10-10 10:15:21 +0400

    Remove https server and self signed certs

--- server/server.js
+++ server/server.js
@@ -1,24 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
+const http = require("http");
 
-const app = require('./src/app');
+const app = require("./src/app");
 
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(
-  {
-    cert: fs.readFileSync('cert/cert.pem'),
-    key: fs.readFileSync('cert/key.pem'),
-  },
-  app
-);
 
 httpServer.listen(8080, function () {
-  console.log('HTTP server is running on port 8080!');
-});
-
-httpsServer.listen(8083, function () {
-  console.log('HTTPS server is running on port 8083!');
+  console.log("HTTP server is running on port 8080!");
 });
